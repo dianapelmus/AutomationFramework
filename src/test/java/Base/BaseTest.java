06a429a594eeb5cfc6da597cf701c2e4f1b54f1e
Diana PELMUS <dpelmus@pentalog.com>
2023-02-02 18:08:27 +0200

    Fix for github actions

--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -1,22 +1,26 @@
 package Base;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseTest {
 
     public WebDriver driver;
 
-    @Before
-
     public void setUp (){
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--disable-gpu");
+        chromeOptions.addArguments("--window-size=1920,1080");
+        chromeOptions.addArguments("--no-sandbox");
+        WebDriverManager.chromedriver().setup();
 
-        //setam driverul pentru Browser
-        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
         //facem o instanta de chrome
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(chromeOptions);
         //accesam un anumit URL
         driver.get("https://demo.automationtesting.in/Index.html");
         //facem instanta maximized
@@ -24,10 +28,4 @@ public class BaseTest {
     }
 
 
-    @After
-
-    public void clearEnvironment (){
-       // driver.quit();
-
-    }
 }
--- src/test/java/Base/Hooks.java
+++ src/test/java/Base/Hooks.java
@@ -1,6 +1,7 @@
 package Base;
 
 import PropertiesFile.PropertiesUtility;
+import org.junit.After;
 import org.junit.Before;
 
 import java.util.HashMap;
@@ -9,9 +10,13 @@ public class Hooks extends BaseTest {
     public HashMap<String,String> testData;
     @Before
     public void prepareEnvironment(){
+        setUp();
         String className = this.getClass().getSimpleName();
         PropertiesUtility propertiesUtility = new PropertiesUtility(className);
         testData = propertiesUtility.getAllKeyValues();
-
+    }
+    @After
+    public void clearEnvironment (){
+        driver.quit();
     }
 }
